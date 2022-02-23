481d2585baf7a033ae5db0a61b4c80482621a215
Alexis Sanchez <53790532+jwlexi@users.noreply.github.com>
2022-02-22 18:12:29 -0800

    application finished

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Alexis Sanchez on 2/22/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
